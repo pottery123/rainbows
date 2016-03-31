5aa744ddba6f408530bff8bdd8d902d071553efc
Sarah <sarahkane682@yahoo.com>
2016-03-31 15:40:26 -0700

    forgot to make a method but here it is it is missing and end though

--- rainbows.rb
+++ rainbows.rb
@@ -42,6 +42,16 @@ rainbow = [{:red => {r: 255, g: 0, b: 0}},
 
 
 
+def print_rainbow
+
+  rainbow = [{:red => {r: 255, g: 0, b: 0}},
+    {:orange => {r: 255, g: 127, b: 0}},
+    {:yellow => {r: 255, g: 255, b: 0}},
+    {:green => {r: 0, g: 255, b: 0}},
+    {:blue => {r: 0, g: 0, b: 255}},
+    {:purple => {r: 75, g: 0, b: 130}},
+    {:violet => {r: 143, g: 0, b: 255}} ]
+
   rainbow.each do |rainbow|
   rainbow.each_key do |k|
     puts k
