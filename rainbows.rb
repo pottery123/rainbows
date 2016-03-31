8996fffa89e778ac97693dcecbb32d219aed0864
Sarah <sarahkane682@yahoo.com>
2016-03-31 10:53:45 -0700

    Actually I figured it out and I did the assignment. Yay

--- rainbows.rb
+++ rainbows.rb
@@ -1,3 +1,4 @@
+=begin
 red = [255,0,0]
 green = [0,255,0]
 blue = [0,0,255]
@@ -28,6 +29,21 @@ end
 new_color = mix_colors(red,green)
 puts "#{color_lookup[red]} #{red} + #{color_lookup[green]} #{green} = #{color_lookup[new_color]} #{new_color}"
 
+=end
 
 
-colors = [[{red => }]   ]
+rainbow = [{:red => {r: 255, g: 0, b: 0}},
+  {:orange => {r: 255, g: 127, b: 0}},
+  {:yellow => {r: 255, g: 255, b: 0}},
+  {:green => {r: 0, g: 255, b: 0}},
+  {:blue => {r: 0, g: 0, b: 255}},
+  {:purple => {r: 75, g: 0, b: 130}},
+  {:violet => {r: 143, g: 0, b: 255}} ]
+
+
+
+  rainbow.each do |rainbow|
+  rainbow.each_key do |k|
+    puts k
+  end
+end
